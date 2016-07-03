398ae1273576132874d57618d5687ed07e8c5eaa
Bastian de Groot <git@de-groot.info>
2016-07-03 11:43:29 +0200

    * separate the easystroke dependencies from the general package list

--- easystroke.sh
+++ easystroke.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+sudo aptitude update
+
+sudo aptitude install build-essential g++ libboost-serialization-dev libgtkmm-3.0-dev libxtst-dev libdbus-glib-1-dev intltool xserver-xorg-dev #build dependency easystroke
 mkdir /tmp/easystroke
-git clone git://github.com/thjaeger/easystroke.git /tmp/easystroke
+git clone https://github.com/thjaeger/easystroke.git /tmp/easystroke
 cd /tmp/easystroke
-make -j2
+make -j4
 sudo make install
 cd "$BASEDIR"
 rm -rf /tmp/easystroke
 
-if [ -f "$HOME/dotfiles/.easystroke" ] ; then
+if [ -d "$HOME/dotfiles/.easystroke" ] ; then
 	cp -vr "$HOME/dotfiles/.easystroke" "$HOME/"
 else
 	echo "dotfiles missing" 1>&2
