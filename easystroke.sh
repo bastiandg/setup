ae466d3e80c74a7df8520e36e01fbfe0f6b93888
Bastian de Groot <git@de-groot.info>
2017-07-08 17:26:13 +0200

    * add patch for easystroke compilation errors

--- easystroke.sh
+++ easystroke.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+TMPDIR="/tmp/easystroke"
 sudo aptitude update
 
 sudo aptitude install -y build-essential g++ libboost-serialization-dev libgtkmm-3.0-dev libxtst-dev libdbus-glib-1-dev intltool xserver-xorg-dev #build dependency easystroke
-mkdir /tmp/easystroke
-git clone https://github.com/thjaeger/easystroke.git /tmp/easystroke
-cd /tmp/easystroke
+git clone https://github.com/thjaeger/easystroke.git "$TMPDIR"
+cp lambda.patch "$TMPDIR"
+cd "$TMPDIR"
+patch < lambda.patch
 make -j4
 sudo make install
 cd "$BASEDIR"
-rm -rf /tmp/easystroke
+rm -rf "$TMPDIR"
 
 if [ -d "$HOME/dotfiles/.easystroke" ] ; then
 	cp -vr "$HOME/dotfiles/.easystroke" "$HOME/"
