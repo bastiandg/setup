832c850448bf3a3e1c09d849221a0b66b53161c6
Bastian de Groot <git@de-groot.info>
2017-07-09 16:05:31 +0200

    * add python neovim plugin

--- neovim.sh
+++ neovim.sh
@@ -18,7 +18,7 @@ trap onexit EXIT
 trap '' INT TERM # Ignore SigINT and SigTERM
 
 sudo aptitude update
-sudo aptitude install -y libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+sudo aptitude install -y libtool libtool-bin autoconf automake cmake g++ pkg-config unzip python3-pip
 
 cd "$TMPDIR"
 curl -L -o neovim.zip "$URL"
@@ -27,12 +27,13 @@ cd neovim-neovim*
 export LC_ALL=C
 make -j4 CMAKE_BUILD_TYPE=Release
 sudo make install
+pip3 install --upgrade neovim
 
 grep 'alias vim="nvim"' "$HOME/.bashrc.local" &> /dev/null
 
 if [ "$?" != "0" ] ; then
-	echo 'alias vim="nvim"' >> "$HOME/.bashrc.local"
-	echo 'alias vi="nvim"' >> "$HOME/.bashrc.local"
+	echo 'alias vim="nvim -u ~/.vimrc"' >> "$HOME/.bashrc.local"
+	echo 'alias vi="vim"' >> "$HOME/.bashrc.local"
 fi
 
 if [ ! -e "$HOME/.config/nvim/" ] ; then
