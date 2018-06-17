1b102c36193687d0e5bcaa22093c28716b96f9f4
Bastian de Groot <git@de-groot.info>
2018-06-17 12:32:09 +0200

    no kde history

---
+++ packages/recent.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+BASEDIR="$(dirname "$(readlink -f "$0")")"
+
+cd "$BASEDIR"
+
+rm -rf "$HOME/.kde/share/apps/RecentDocuments/"*
+rm -f "$HOME/.local/share/recently-used.xbel"
+echo "" > "$HOME/.local/share/recently-used.xbel"
+rm -rf "$HOME/.local/share/kactivitymanagerd/resources/"*
+
+cp -f "../files/gwenviewrc" "$HOME/.config/"
+cp -f "../files/org.kde.gwenviewrc" "$HOME/.config/"
+
+chmod -w "$HOME/.local/share/recently-used.xbel"
+chmod -w "$HOME/.kde/share/apps/RecentDocuments/"
+chmod -w "$HOME/.local/share/kactivitymanagerd/resources/"
+chmod -w "$HOME/.config/gwenviewrc"
+chmod -w "$HOME/.config/org.kde.gwenviewrc"
