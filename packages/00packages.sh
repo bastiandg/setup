ae5fba52c2df959a82cf17919ebdcfe2ac8b7d03
Bastian de Groot <git@de-groot.info>
2018-09-14 16:34:18 +0200

    apt replaces aptitude

--- packages/00packages.sh
+++ packages/00packages.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-INSTALL="sudo aptitude install -y"
+INSTALL="sudo apt install -y"
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 PACKAGELIST="$BASEDIR/../files/package.list"
 
-sudo aptitude update
+sudo apt update
 $INSTALL $(grep -v "^#" "$PACKAGELIST" | sed -e "s/\(.*\)#.*/\1/g" | tr "\\n" " ")
 
