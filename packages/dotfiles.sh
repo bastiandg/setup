405c970ec24b63cebdddf9c8a004ec1ced59f963
Bastian de Groot <git@de-groot.info>
2018-09-14 16:34:46 +0200

    fix update_dotfiles path

--- packages/dotfiles.sh
+++ packages/dotfiles.sh
@@ -4,5 +4,5 @@ if [ ! -d "$HOME/dotfiles" ] ; then
 	git clone --recursive -j8 https://github.com/bastiandg/dotfiles.git "$HOME/dotfiles"
 fi
 
-source "$HOME/dotfiles/.bash/functions/99_update_dotfiles.sh"
+source "$HOME/dotfiles/bash/functions/99_update_dotfiles.sh"
 update_dotfiles
