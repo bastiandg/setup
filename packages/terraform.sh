a0d361e33bb46106ef5f5ec0437d4b27525c50ca
Bastian de Groot <git@de-groot.info>
2018-09-15 13:58:38 +0200

    Enhance error handling and add versioncheck to terraform

--- packages/terraform.sh
+++ packages/terraform.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
-set -e
+set -eu -o pipefail
 
 BASEDIR="$(dirname "$(readlink -f "$0")")"
+RELEASE_URL="https://releases.hashicorp.com/terraform/"
+RELEASE_PAGE="$(curl -s "$RELEASE_URL")"
+VERSION="$(echo "$RELEASE_PAGE" | grep -v 'rc\|beta' | grep -m 1 'href="/terraform' | sed -re "s#.*terraform_([0-9.]*)</a>#\1#")"
+
+versioncheck () {
+	set +eu
+	localversion="$(terraform --version 2> /dev/null | grep -oP '(?<=Terraform v).*(?=$)')"
+	set -eu
+	remoteversion="$VERSION"
+	if [ "$localversion" = "$remoteversion" ] ; then
+		echo "terraform is up to date"
+		exit 0
+	fi
+}
+
+versioncheck
 TMPDIR="$(mktemp -d)"
-VERSION="$(curl "https://releases.hashicorp.com/terraform/" 2> /dev/null | grep -v 'rc\|beta' | grep -m 1 'href="/terraform' | sed -re "s#.*terraform_([0-9.]*)</a>#\1#")"
 echo "Installing terraform $VERSION"
 cd "$TMPDIR"
-curl -o terraform.zip "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip"
+curl -o terraform.zip "${RELEASE_URL}${VERSION}/terraform_${VERSION}_linux_amd64.zip"
 unzip terraform.zip
 sudo mv terraform /usr/bin/
 cd "$BASEDIR"
