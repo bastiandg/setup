f976a4d92e2dec678f81c3bfa0bfbfe21344cb11
Bastian de Groot <git@de-groot.info>
2018-09-15 13:18:51 +0200

    Enhance error handling and include versioncheck for firefox

--- packages/firefox.sh
+++ packages/firefox.sh
@@ -1,20 +1,38 @@
 #!/usr/bin/env bash
 
-INSTALLDIR="/opt/"
+set -eu -o pipefail
+
+INSTALLDIR="/opt/firefox"
 PUID="1000" #user who "owns" the software
 BASEDIR="$(dirname "$(readlink -f "$0")")"
+DOWNLOADURL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=de"
 
-if [ ! -d "$INSTALLDIR/firefox" ] ; then
-	DOWNLOADURL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=de"
+versioncheck () {
+	set +eu
+	localversion="$(firefox --version 2> /dev/null | grep -oP '(?<=Mozilla Firefox ).*(?=$)')"
+	set -eu
+	remoteversion="$(curl -s "$DOWNLOADURL" | grep -oP '(?<=releases/).*(?=.linux-x86_64)')"
+	if [ "$localversion" = "$remoteversion" ] ; then
+		echo "firefox is up to date"
+		exit 0
+	fi
+}
 
-	mkdir -p /tmp/firefox
-	cd /tmp/firefox
-	wget "$DOWNLOADURL" -O firefox.tar.bz2
-	tar xvjf firefox.tar.bz2
-	sudo cp -vr firefox "$INSTALLDIR/firefox"
-	sudo chown -R "$PUID":"$PUID" "$INSTALLDIR/firefox"
-	sudo ln -s "$INSTALLDIR/firefox/firefox" /usr/bin/firefox
-	cd "$BASEDIR"
-	rm -rf /tmp/firefox
-	sudo cp "$BASEDIR/../files/firefox.desktop" "/usr/share/applications/firefox.desktop"
+versioncheck
+if [ -n "$(dpkg-query -W -f='${Status}' firefox-esr | grep "\<installed\>" || true)" ] ; then
+	sudo apt remove -y firefox-esr
 fi
+
+if [ -d "$INSTALLDIR" ] ; then
+	sudo rm -r "$INSTALLDIR"
+fi
+TMPDIR="$(mktemp -d)"
+cd "$TMPDIR"
+wget "$DOWNLOADURL" -O firefox.tar.bz2
+tar xvjf firefox.tar.bz2
+sudo cp -vr firefox "$INSTALLDIR"
+sudo chown -R "$PUID:$PUID" "$INSTALLDIR"
+sudo ln -sf "$INSTALLDIR/firefox" /usr/bin/firefox
+cd "$BASEDIR"
+rm -rf "$TMPDIR"
+sudo cp "$BASEDIR/../files/firefox.desktop" "/usr/share/applications/firefox.desktop"
