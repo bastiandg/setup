4cfb4753748ed76d0e6bbdba115c78db5d8741b4
Bastian de Groot <git@de-groot.info>
2018-09-15 11:55:12 +0200

    fix easystroke issues with abs-patch; remove gnome-do; fix update_dotfiles patching; replace aptitude with apt

--- packages/easystroke.sh
+++ packages/easystroke.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 TMPDIR="$(mktemp -d)"
 URL="https://github.com/thjaeger/easystroke.git"
-sudo aptitude update
+sudo apt update
 
-sudo aptitude install -y build-essential g++ libboost-serialization-dev libgtkmm-3.0-dev libxtst-dev libdbus-glib-1-dev intltool xserver-xorg-dev #build dependency easystroke
+sudo apt install -y build-essential g++ libboost-serialization-dev libgtkmm-3.0-dev libxtst-dev libdbus-glib-1-dev intltool xserver-xorg-dev #build dependency easystroke
 git clone "$URL" "$TMPDIR"
-if lsb_release -c | grep jessie ; then
-	cd "$TMPDIR"
-else
-	cp "$BASEDIR/../files/lambda.patch" "$TMPDIR"
-	cd "$TMPDIR"
-	patch < lambda.patch
-fi
+cp "$BASEDIR/../files/lambda.patch" "$TMPDIR"
+cp "$BASEDIR/../files/abs.patch" "$TMPDIR"
+cd "$TMPDIR"
+patch < lambda.patch
+patch < abs.patch
 make -j4
 sudo make install
 cd "$BASEDIR"
