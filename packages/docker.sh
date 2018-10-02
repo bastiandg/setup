b7eb53eb586671026227cc3b15ba30f0d2536867
Bastian de Groot <git@de-groot.info>
2018-10-02 15:23:14 +0200

    Handle aufs errors

--- packages/docker.sh
+++ packages/docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 sudo apt-get update
 sudo apt-get -y install \
      apt-transport-https \
@@ -9,8 +11,14 @@ sudo apt-get -y install \
      software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce
+
+# The install might return a non zero return code because aufs is not initialized yet
+sudo apt-get install -y docker-ce || true
+
+if ! sudo systemctl --no-pager status docker.service > /dev/null ; then
+    sudo systemctl start docker.service
+fi
