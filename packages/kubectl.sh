ddb67b8fb5b99e0d787d6bf158e12a0e2c61876e
Bastian de Groot <git@de-groot.info>
2018-11-15 15:09:09 +0100

    install kubectl only in case there is an update

--- packages/kubectl.sh
+++ packages/kubectl.sh
@@ -2,8 +2,19 @@
 set -e
 DESTINATION="/usr/local/bin/kubectl"
 VERSION_URL="https://storage.googleapis.com/kubernetes-release/release/stable.txt"
-VERSION="$(curl -s "$VERSION_URL")"
-PACKAGE_URL="https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubectl"
+REMOTE_VERSION="$(curl -s "$VERSION_URL")"
+PACKAGE_URL="https://storage.googleapis.com/kubernetes-release/release/$REMOTE_VERSION/bin/linux/amd64/kubectl"
 
+versioncheck () {
+	set +eu
+	LOCAL_VERSION="$(kubectl version --short --client 2> /dev/null | sed -e "s#Client Version: ##g")"
+	set -eu
+	if [ "$LOCAL_VERSION" == "$REMOTE_VERSION" ] ; then
+		echo "kubectl is up to date"
+		exit 0
+	fi
+}
+
+versioncheck
 sudo curl -o "$DESTINATION" -LO "$PACKAGE_URL"
 sudo chmod +x "$DESTINATION"
