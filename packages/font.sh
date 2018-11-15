baa3f3e0fc11677b31106976169cb722184ab573
Bastian de Groot <git@de-groot.info>
2018-11-15 15:07:37 +0100

    Explicitly download only otf version of sourcecodepro

--- packages/font.sh
+++ packages/font.sh
@@ -3,11 +3,10 @@
 FONT_NAME="SourceCodePro"
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 TMPDIR="$(mktemp -d)"
-URL="$(curl "https://api.github.com/repos/adobe-fonts/source-code-pro/releases/latest" 2> /dev/null | jq -r ".zipball_url")"
+URL="$(curl "https://api.github.com/repos/adobe-fonts/source-code-pro/releases/latest" 2> /dev/null | jq -r '.assets[] | select(.name | contains("Variable-Roman.otf")) | .browser_download_url')"
 
 cd "$TMPDIR"
-curl -L -o "${FONT_NAME}.zip" "${URL}"
-unzip -o -j "${FONT_NAME}.zip"
+curl -L -o "${FONT_NAME}.otf" "${URL}"
 mkdir -p "$HOME/.fonts"
 cp -- *.otf "$HOME/.fonts"
 fc-cache -f -v
