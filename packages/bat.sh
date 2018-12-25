c1155cd52871b1f20251b1e07a94a087f620f9db
Bastian de Groot <git@de-groot.info>
2018-12-25 21:55:21 +0100

    Add bat

---
+++ packages/bat.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+BASEDIR="$(dirname "$(readlink -f "$0")")"
+RELEASES="$(curl -s "https://api.github.com/repos/sharkdp/bat/releases/latest")"
+URL="$(echo "$RELEASES" | jq -r '.assets[] | select(.browser_download_url | test("bat_.*amd64.deb")) | .browser_download_url')"
+
+
+versioncheck () {
+	localversion="$(bat --version 2> /dev/null | cut -d " " -f2 || echo "NULL")"
+	remoteversion="$(echo "$RELEASES" | jq -r ".name")"
+	if [ "v$localversion" = "$remoteversion" ] ; then
+		echo "bat is up to date (version $localversion)"
+		exit 0
+	else
+		echo "updating bat from $localversion to $remoteversion"
+	fi
+}
+
+versioncheck
+
+TMPDIR="$(mktemp -d)"
+cd "$TMPDIR"
+curl -Ls -o bat.deb "$URL"
+sudo dpkg -i bat.deb
+cd "$BASEDIR"
+rm -rf "$TMPDIR"
