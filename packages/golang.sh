e11a2d77b7dbeefe02dcbce9758f4fb19dd3c8ac
Bastian de Groot <git@de-groot.info>
2019-03-08 10:05:41 +0100

    Add vscode in form of vscodium

--- packages/golang.sh
+++ packages/golang.sh
@@ -4,7 +4,7 @@ set -e
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 
 versioncheck () {
-	localversion="$(go version | sed -re "s#go version go(.*) .*#\1#g")"
+	localversion="$(go version | sed -re 's#go version go(.*) .*#\1#g')"
 	remoteversion="$(curl "https://golang.org/dl/" 2> /dev/null | grep -oP '(?<=<a class="download downloadBox" href="https://dl.google.com/go/go).*(?=.linux-amd64.tar.gz">)')"
 	if [ "$localversion" = "$remoteversion" ] ; then
 		echo "Go is up to date"
---
+++ packages/vscode.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+BASEDIR="$(dirname "$(readlink -f "$0")")"
+URL="$(curl "https://api.github.com/repos/VSCodium/vscodium/releases/latest" 2> /dev/null | jq -r '.assets[] | select(.browser_download_url | contains ("amd64.deb")) | .browser_download_url')"
+
+versioncheck () {
+	localversion="$( (vscodium --version | head -1) || true)"
+	remoteversion="$(curl "https://api.github.com/repos/VSCodium/vscodium/releases/latest" 2> /dev/null | jq -r ".name")"
+	if [ "$localversion" = "$remoteversion" ] ; then
+		echo "vscodium is up to date ($localversion)"
+		exit 0
+	fi
+}
+
+versioncheck
+TMPDIR="$(mktemp -d)"
+
+cd "$TMPDIR"
+curl -L -o "vscodium.deb" "${URL}"
+sudo dpkg -i "vscodium.deb"
+
+cd "$BASEDIR"
+rm -rf "$TMPDIR"
