33707cf7758c80b6e245dc08ab2676e55f6df4ec
Bastian de Groot <git@de-groot.info>
2019-09-04 08:59:38 +0200

    No URL debugging message

--- packages/vscodium.sh
+++ packages/vscodium.sh
@@ -4,8 +4,6 @@ set -eu -o pipefail
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 URL="$(curl "https://api.github.com/repos/VSCodium/vscodium/releases/latest" 2> /dev/null | jq -r '.assets[] | select(.browser_download_url | endswith("amd64.deb")) | .browser_download_url')"
 
-echo "$URL"
-
 versioncheck () {
 	localversion="$( (codium --version | head -1) || true)"
 	remoteversion="$(curl "https://api.github.com/repos/VSCodium/vscodium/releases/latest" 2> /dev/null | jq -r ".name")"
