1dd4897cfcd1246e365f3983fcc89533c34d1fcf
Bastian de Groot <Bastian de Groot>
2020-10-13 11:59:18 +0200

    shfmt

---
+++ packages/shfmt.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+response="$(curl -s "https://api.github.com/repos/mvdan/sh/releases/latest")"
+download_url="$(jq -r '.assets[] | select(.browser_download_url | contains("linux_amd64")) | .browser_download_url' <<< "$response")"
+#-linux_amd64
+
+versioncheck () {
+	localversion="$( shfmt --version 2> /dev/null || true)"
+	remoteversion="$(jq -r ".name" <<< "$response")"
+	if [ "$localversion" = "$remoteversion" ] ; then
+		echo "shfmt is up to date ($localversion)"
+		exit 0
+	fi
+}
+
+versioncheck
+sudo curl -L -o "/usr/local/bin/shfmt" "$download_url"
+sudo chmod +x /usr/local/bin/shfmt
