2cfc7bfbfe2006726ab2a3e3e2ce572743123a44
Bastian de Groot <git@de-groot.info>
2020-10-18 13:03:00 +0200

    KeePassRPC update script

---
+++ packages/keepassrpc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+response="$(curl -s "https://api.github.com/repos/kee-org/keepassrpc/releases/latest")"
+download_url="$(jq -r '.assets[] | select(.browser_download_url | endswith("plgx")) | .browser_download_url' <<< "$response")"
+filename="$(basename "$download_url")"
+
+sudo curl -L -o "/usr/lib/keepass2/${filename}" "$download_url"
