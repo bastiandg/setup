9c9c8344f6d6c0ca844d954be9df94c9832dac32
Bastian de Groot <Bastian de Groot>
2020-11-19 14:21:35 +0100

    remove exit in helm install

--- packages/helm.sh
+++ packages/helm.sh
@@ -5,24 +5,23 @@ set -eu -o pipefail
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 RELEASE_URL="https://api.github.com/repos/helm/helm/releases/latest"
 RESPONSE="$(curl -sL "$RELEASE_URL")"
-VERSION="$(jq -r '.tag_name' <<< "$RESPONSE")"
+VERSION="$(jq -r '.tag_name' <<<"$RESPONSE")"
 DOWNLOAD_URL="https://get.helm.sh/helm-${VERSION}-linux-amd64.tar.gz"
 
-versioncheck () {
-	localversion="$(helm version --short 2> /dev/null | sed -re "s/(.*)\+.*/\1/g" || true)"
-	remoteversion="$(jq -r ".tag_name" <<< "$RESPONSE")"
-	if [[ "$localversion" == "$remoteversion" ]] ; then
-		echo "helm is up to date ($localversion)"
-		exit 0
-	else
-		echo "helm will be updated from '$localversion' to '$remoteversion'"
-	fi
+versioncheck() {
+  localversion="$(helm version --short 2>/dev/null | sed -re "s/(.*)\+.*/\1/g" || true)"
+  remoteversion="$(jq -r ".tag_name" <<<"$RESPONSE")"
+  if [[ "$localversion" == "$remoteversion" ]]; then
+    echo "helm is up to date ($localversion)"
+    exit 0
+  else
+    echo "helm will be updated from '$localversion' to '$remoteversion'"
+  fi
 }
 
 versioncheck
 TMPDIR="$(mktemp -d)"
 
-exit
 cd "$TMPDIR"
 curl -L -o "helm.tar.gz" "${DOWNLOAD_URL}"
 tar zxf helm.tar.gz linux-amd64/helm -C .
