16ec6b9949bf702a8d11519aa210c2804875eda4
Bastian de Groot <Bastian de Groot>
2020-11-19 14:05:20 +0100

    Update sc pro to new release structure

--- packages/font.sh
+++ packages/font.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-FONT_NAME="SourceCodePro"
+set -eu -o pipefail
+
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 TMPDIR="$(mktemp -d)"
-URL="$(curl "https://api.github.com/repos/adobe-fonts/source-code-pro/releases/latest" 2> /dev/null | jq -r '.assets[] | select(.name | contains("Variable-Roman.otf")) | .browser_download_url')"
+URL="$(curl "https://api.github.com/repos/adobe-fonts/source-code-pro/releases/latest" 2>/dev/null | jq -r '.assets[] | select(.name | startswith("OTF-")) | .browser_download_url')"
 
 cd "$TMPDIR"
-curl -L -o "${FONT_NAME}.otf" "${URL}"
+curl -L -o "sc-pro.zip" "${URL}"
+unzip "sc-pro.zip"
+cp -- OTF/*.otf "$HOME/.fonts"
 mkdir -p "$HOME/.fonts"
-cp -- *.otf "$HOME/.fonts"
 fc-cache -f -v
 
 cd "$BASEDIR"
 rm -rf "$TMPDIR"
-
