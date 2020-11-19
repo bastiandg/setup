d8c211389675a9fbd25f5e2b67b8b1d64c54b8b8
Bastian de Groot <Bastian de Groot>
2020-11-19 14:39:51 +0100

    Use precompile nvim binary

--- packages/neovim.sh
+++ packages/neovim.sh
@@ -2,14 +2,17 @@
 
 set -eu -o pipefail
 
+INSTALL_PATH="/opt/nvim"
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 RELEASE_URL="https://api.github.com/repos/neovim/neovim/releases/latest"
+response="$(curl -s "$RELEASE_URL")"
+download_url="$(jq -r '.assets[] | select(.name | endswith("linux64.tar.gz")) | .browser_download_url' <<<"$response")"
 
 versioncheck() {
-  set +eu
+  set +e
   localversion="$(nvim --version 2>/dev/null | grep -oP '(?<=NVIM ).*(?=$)')"
-  set -eu
-  remoteversion="$(curl -s "$RELEASE_URL" | jq -r ".name" | grep -oP '(?<=NVIM ).*(?=$)')"
+  set -e
+  remoteversion="$(jq -r ".name" <<<"$response" | grep -oP '(?<=NVIM ).*(?=$)')"
   if [ "$localversion" = "v$remoteversion" ]; then
     echo "neovim is up to date (version $localversion)"
     exit 0
@@ -34,22 +37,10 @@ onexit() {
 trap onexit EXIT
 trap '' INT TERM # Ignore SigINT and SigTERM
 
-sudo apt-get update -y
-sudo apt-get install -y jq curl libtool libtool-bin autoconf automake cmake g++ pkg-config unzip python3-pip
+tmpdir="$(mktemp -d)"
 
-TMPDIR="$(mktemp -d)"
-URL="$(curl -s "$RELEASE_URL" | jq -r ".zipball_url")"
-
-cd "$TMPDIR"
-curl -L -o neovim.zip "$URL"
-unzip -o neovim.zip
-cd neovim-neovim*
-export LC_ALL=C
-make -j"$(nproc --all)" CMAKE_BUILD_TYPE=Release
-sudo make install
-pip3 install --upgrade neovim
-
-if [ ! -e "$HOME/.config/nvim/" ]; then
-  cp -r "$HOME/.vim" "$HOME/.config/nvim"
-  cp "$HOME/.vimrc" "$HOME/.config/nvim/init.vim"
-fi
+cd "$tmpdir"
+curl -L -o neovim.tgz "$download_url"
+tar zxf neovim.tgz
+sudo mv nvim-linux64 "$INSTALL_PATH"
+sudo ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
