0594a9fbb65122c3065c4de993ac3895799c5640
Bastian de Groot <Bastian de Groot>
2020-11-19 14:19:41 +0100

    Adapt golang install script to new page structure

--- packages/golang.sh
+++ packages/golang.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
-set -e
+
+set -eu -o pipefail
 
 BASEDIR="$(dirname "$(readlink -f "$0")")"
+version="$(curl "https://golang.org/dl/" 2>/dev/null | grep -oP '(?<=<a class="download downloadBox" href="/dl/go).*(?=.linux-amd64.tar.gz">)')"
+download_url="https://golang.org/dl/go${version}.linux-amd64.tar.gz"
 
-versioncheck () {
-	localversion="$(go version | sed -re 's#go version go(.*) .*#\1#g')"
-	remoteversion="$(curl "https://golang.org/dl/" 2> /dev/null | grep -oP '(?<=<a class="download downloadBox" href="https://dl.google.com/go/go).*(?=.linux-amd64.tar.gz">)')"
-	if [ "$localversion" = "$remoteversion" ] ; then
-		echo "Go is up to date"
-		exit 0
-	fi
+versioncheck() {
+  localversion="$(go version | sed -re 's#go version go(.*) .*#\1#g')"
+  remoteversion="$version"
+  if [[ "$localversion" = "$remoteversion" ]]; then
+    echo "Go is up to date ($version)"
+    exit 0
+  fi
 }
 
 versioncheck
 TMPDIR="$(mktemp -d)"
-URL="$(curl "https://golang.org/dl/" 2> /dev/null | grep -oP '(?<=<a class="download downloadBox" href=").*linux-amd64.tar.gz(?=">)')"
 
 cd "$TMPDIR"
-curl -L -o go.tgz "${URL}"
+curl -L -o go.tgz "$download_url"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go.tgz
 sudo ln -sf /usr/local/go/bin/go /usr/local/bin/go
