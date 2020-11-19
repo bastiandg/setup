533419d2e68ae7edcd9022d3b657c7e9983c7cd7
Bastian de Groot <Bastian de Groot>
2020-11-19 14:41:55 +0100

    Update installed packages for work pcs

--- work.sh
+++ work.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 LOG="install.log"
 exec &> >(tee "$LOG") 2>&1
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 
 "$BASEDIR/packages/00packages.sh"
-"$BASEDIR/packages/diff-so-fancy.sh"
-"$BASEDIR/packages/easystroke.sh"
-"$BASEDIR/packages/emoji.sh"
+#"$BASEDIR/packages/emoji.sh"
 "$BASEDIR/packages/firefox.sh"
 "$BASEDIR/packages/font.sh"
 "$BASEDIR/packages/gitconfig.sh"
 "$BASEDIR/packages/golang.sh"
+"$BASEDIR/packages/helm.sh"
 "$BASEDIR/packages/kubectl.sh"
-"$BASEDIR/packages/rocket.sh"
 "$BASEDIR/packages/terraform.sh"
 "$BASEDIR/packages/thunderbird.sh"
 
 # IDE
-"$BASEDIR/packages/dotfiles.sh"
 "$BASEDIR/packages/neovim.sh"
+"$BASEDIR/packages/delta.sh"
+"$BASEDIR/packages/shfmt.sh"
 "$BASEDIR/packages/fzf.sh"
 "$BASEDIR/packages/mdl.sh"
 "$BASEDIR/packages/hadolint.sh"
+"$BASEDIR/packages/dotfiles.sh"
