7f8a9904ab8781f3ba3e7a6073a33e9fd082d65a
Bastian de Groot <Bastian de Groot>
2021-01-05 14:37:31 +0100

    Set neovim to latest nightly for lsp

--- packages/neovim.sh
+++ packages/neovim.sh
@@ -4,16 +4,18 @@ set -eu -o pipefail
 
 INSTALL_PATH="/opt/nvim"
 BASEDIR="$(dirname "$(readlink -f "$0")")"
-RELEASE_URL="https://api.github.com/repos/neovim/neovim/releases/latest"
+RELEASE_URL="https://api.github.com/repos/neovim/neovim/releases"
+TAG_NAME="nightly" # TODO switch back to stable when nvim 0.5 is released
+#TAG_NAME="stable"
 response="$(curl -s "$RELEASE_URL")"
-download_url="$(jq -r '.assets[] | select(.name | endswith("linux64.tar.gz")) | .browser_download_url' <<<"$response")"
+download_url="$(jq -r '.[] | select(.tag_name | test("'"$TAG_NAME"'")) | .assets[] | select(.name | endswith("linux64.tar.gz")) | .browser_download_url' <<<"$response")"
 
 versioncheck() {
   set +e
-  localversion="$(nvim --version 2>/dev/null | grep -oP '(?<=NVIM ).*(?=$)')"
+  localversion="$(nvim --version 2>/dev/null | grep -oP '(?<=NVIM ).*(?=$)' || true)"
   set -e
-  remoteversion="$(jq -r ".name" <<<"$response" | grep -oP '(?<=NVIM ).*(?=$)')"
-  if [ "$localversion" = "v$remoteversion" ]; then
+  remoteversion="$(jq -r '.[] | select(.tag_name | test("'"$TAG_NAME"'"))  | .name' <<<"$response" | grep -oP '(?<=NVIM ).*(?=$)' || true)"
+  if [[ "$localversion" == "v$remoteversion" ||  "$localversion" == "$remoteversion" ]]; then
     echo "neovim is up to date (version $localversion)"
     exit 0
   else
@@ -42,5 +44,6 @@ tmpdir="$(mktemp -d)"
 cd "$tmpdir"
 curl -L -o neovim.tgz "$download_url"
 tar zxf neovim.tgz
+sudo rm -r "$INSTALL_PATH"
 sudo mv nvim-linux64 "$INSTALL_PATH"
 sudo ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
