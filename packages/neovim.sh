da76fc3a4b76861fa5f03d93ca7b59fb10d1f485
Bastian de Groot <git@de-groot.info>
2025-08-19 10:27:15 +0200

    Update neovim install

--- packages/neovim.sh
+++ packages/neovim.sh
@@ -4,16 +4,15 @@ set -eu -o pipefail
 
 INSTALL_PATH="/opt/nvim"
 BASEDIR="$(dirname "$(readlink -f "$0")")"
-RELEASE_URL="https://api.github.com/repos/neovim/neovim/releases"
-TAG_NAME="stable"
+RELEASE_URL="https://api.github.com/repos/neovim/neovim/releases/latest"
 response="$(curl -s "$RELEASE_URL")"
-download_url="$(jq -r '.[] | select(.tag_name | test("'"$TAG_NAME"'")) | .assets[] | select(.name | endswith("linux64.tar.gz")) | .browser_download_url' <<<"$response")"
+download_url="$(jq -r '.assets[] | select(.name | endswith("linux-x86_64.tar.gz")) | .browser_download_url' <<<"$response")"
 
 versioncheck() {
   set +e
   localversion="$(nvim --version 2>/dev/null | grep -oP '(?<=NVIM ).*(?=$)' || true)"
   set -e
-  remoteversion="$(jq -r '.[] | select(.tag_name | test("'"$TAG_NAME"'"))  | .name' <<<"$response" | grep -oP '(?<=NVIM ).*(?=$)' || true)"
+  remoteversion="$(jq -r '.tag_name' <<<"$response" || true)"
   if [[ $localversion == "v$remoteversion" || $localversion == "$remoteversion" ]]; then
     echo "neovim is up to date (version $localversion)"
     exit 0
@@ -27,10 +26,10 @@ versioncheck
 # setup exit handler
 #################################################################################
 onexit() {
-  cd "$BASEDIR"
+  cd -- "$BASEDIR"
   echo "Script is terminating -- cleaning up"
-  if [[ -d ${TMPDIR:-} ]]; then
-    sudo rm -rf "$TMPDIR"
+  if [[ -d ${tmpdir:-} ]]; then
+    sudo rm -rf "$tmpdir"
   fi
   exit
 }
@@ -45,5 +44,5 @@ curl -L -o neovim.tgz "$download_url"
 tar zxf neovim.tgz
 sudo rm -r "$INSTALL_PATH" || true
 #sudo mkdir -p "$INSTALL_PATH"
-sudo mv nvim-linux64 "$INSTALL_PATH"
-sudo ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
+sudo mv nvim-linux-x86_64/ "$INSTALL_PATH"
+sudo ln -sf "${INSTALL_PATH}/bin/nvim" /usr/local/bin/nvim
